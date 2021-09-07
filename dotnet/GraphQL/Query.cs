98df88d0b2bc31f1728f6dbe94df4dda5bfa1236
Brian Talma <brian.talma@vtex.com.br>
2021-09-07 15:38:30 -0400

    graphql

---
+++ dotnet/GraphQL/Query.cs
@@ -0,0 +1,15 @@
+﻿using Cybersource.Services;
+using GraphQL;
+using GraphQL.Types;
+
+namespace Cybersource.GraphQL
+{
+    [GraphQLMetadata("Query")]
+    public class Query : ObjectGraphType<object>
+    {
+        public Query(IVtexApiService vtexApiService)
+        {
+            Name = "Query";
+        }
+    }
+}
--- dotnet/Models/MerchantSettings.cs
+++ dotnet/Models/MerchantSettings.cs
@@ -12,6 +12,7 @@ namespace Cybersource.Models
         public string SharedSecretKey { get; set; }
         public string Processor { get; set; }
         public string Region { get; set; }
+        // Taxes
         public bool EnableTransactionPosting { get; set; }
         public string SalesChannelExclude { get; set; }
     }
