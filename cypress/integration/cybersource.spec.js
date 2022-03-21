d6398209603b6966a31ffd2bf4c11e56a5e55c6d
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-03-21 14:22:25 +0530

    Added orderTaxAPI & cybersource testcases

---
+++ cypress/integration/cybersource.spec.js
@@ -0,0 +1,5 @@
+describe('Cybersource Testcase', () => {
+  it('Test cybersource API', () => {
+    cy.cybersourceapi('6476351198506781104003')
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,9 +5,59 @@ import {
   updateShippingInformation,
   getIframeBody,
 } from './cypress-template/common_support.js'
+import {
+  VTEX_AUTH_HEADER,
+  FAIL_ON_STATUS_CODE,
+} from './cypress-template/common_constants.js'
 
 Cypress.Commands.add('searchProduct', searchProduct)
 Cypress.Commands.add('addProduct', addProduct)
 Cypress.Commands.add('updateShippingInformation', updateShippingInformation)
 Cypress.Commands.add('getIframeBody', getIframeBody)
 Cypress.Commands.add('fillAddress', fillAddress)
+
+// Order Tax API Test Case
+Cypress.Commands.add('orderTaxApi', (requestPayload, tax) => {
+  cy.getVtexItems().then(vtex => {
+    cy.request({
+      method: 'POST',
+      url: `${vtex.baseUrl}/cybersource/checkout/order-tax`,
+      headers: {
+        Authorization: vtex.authorization,
+        ...VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+      },
+      ...FAIL_ON_STATUS_CODE,
+      body: requestPayload,
+    }).then(response => {
+      expect(response.status).to.equal(200)
+
+      const reqIds = requestPayload.items.map(({ id }) => id)
+      const respIds = response.body.itemTaxResponse.map(({ id }) => id)
+
+      expect(reqIds.sort()).to.deep.equal(respIds.sort())
+      let taxFromAPI = 0
+
+      response.body.itemTaxResponse.forEach(item => {
+        item.taxes.map(obj => (taxFromAPI += obj.value))
+      })
+      expect(taxFromAPI.toFixed(2)).to.equal(tax.replace('$ ', ''))
+    })
+  })
+})
+
+Cypress.Commands.add('cybersourceapi', (tid, fn = null) => {
+  cy.getVtexItems().then(vtex => {
+    cy.request({
+      url: `${vtex.cybersourceapi}/${tid}`,
+      headers: {
+        signature: vtex.signature,
+        'v-c-merchant-id': vtex.merchantId,
+        'v-c-date': new Date().toUTCString(),
+      },
+      ...FAIL_ON_STATUS_CODE,
+    }).then(resp => {
+      expect(resp.status).to.equal(200)
+      fn && fn()
+    })
+  })
+})
